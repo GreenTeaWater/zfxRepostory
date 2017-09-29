bbdb029b3118f788b66a9d7df592d2774217a81a
zhaofx <526183297@qq.com>
2017-09-29 18:14:03 +0800

    webservice

---
+++ weChat/src/main/java/com/wechat/common/util/weChatConstant.java
@@ -0,0 +1,17 @@
+package com.wechat.common.util;
+
+public class weChatConstant {
+
+	public static final String home = "您好，请按一下内容回复查询！" + weChatConstant.rn
+									+ "ip=输入要查询的ip" + weChatConstant.rn
+									+ "天气=输入要查询的城市";
+	
+	public static final String ip = "ip";
+	public static final String weather = "天气";
+
+	public static final String error = "服务异常，请稍后再试";
+
+	public static final String rn = "\r\n";
+
+	
+}
---
+++ weChat/src/main/java/com/wechat/common/util/weChatUtils.java
@@ -0,0 +1,81 @@
+package com.wechat.common.util;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Iterator;
+
+import org.codehaus.xfire.client.Client;
+import org.dom4j.Element;
+import org.w3c.dom.Document;
+
+public class weChatUtils {
+
+	
+	public static String getIp(String ip){
+		String result = "" ;
+		try {
+			Client client = new Client(new URL("http://www.webxml.com.cn/WebServices/IpAddressSearchWebService.asmx?wsdl"));
+	    	Object [] req = new Object[1];
+	    	req[0] = ip ;
+	    	
+	    	Object[] results = client.invoke("getCountryCityByIp", req);
+	    	String ips = null ,address = null ;
+	    	for(Object obj:results){
+	    		Document document = (Document)obj ;
+	    		org.dom4j.io.DOMReader xmlReader = new org.dom4j.io.DOMReader();  
+	    		org.dom4j.Document document2 = xmlReader.read(document);
+	    		Iterator iterator = document2.getRootElement().elementIterator();
+	    		Element ele = (Element) iterator.next() ;
+	    		ips = ele.getText();
+	    		Element eleaddr = (Element) iterator.next() ;
+	    		address = eleaddr.getText() ;
+	    	}
+	    	return "IP=" + ips + weChatConstant.rn
+	    			+ "地址=" + address ;
+		} catch (Exception e) {
+				result = weChatConstant.error;
+				e.printStackTrace(); 
+		}
+		return result;
+	
+	}
+
+	public static String getweather(String str) {
+		String result = "" ;
+		try {									
+			Client client = new Client(new URL("http://ws.webxml.com.cn/WebServices/WeatherWS.asmx?wsdl"));
+	    	Object [] req = new Object[3];
+	    	req[0] = str ;
+	    	req[1] = "240bc824eb72477a83424a825932ed0d" ;
+	    	Object[] results = client.invoke("getWeather", req);
+	    	
+	    	for(Object obj:results){
+	    		Document document = (Document)obj ;
+	    		org.dom4j.io.DOMReader xmlReader = new org.dom4j.io.DOMReader();  
+	    		org.dom4j.Document document2 = xmlReader.read(document);
+	    		Iterator iterator = document2.getRootElement().elementIterator();
+	    		while(iterator.hasNext()){
+	    			Element ele = (Element) iterator.next() ;
+	    			if(ele.getText().contains("今日天气实况")){
+	    				result = ele.getText() ;
+	    			}
+	    		}
+	    		
+	    		
+	    	}
+	    	return "城市=" + str + weChatConstant.rn
+	    			+ "天气=" + result ;
+		} catch (Exception e) {
+				result = weChatConstant.error;
+				e.printStackTrace(); 
+		}
+		return result;
+	
+	}
+	
+	
+	public static void main(String[] args) {
+		System.out.println(getweather("785"));
+		
+	}
+}
